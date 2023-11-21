4a5d2a3c389d6e1a5cbcbd3826445b4d111d6203
Lekahelis <itnaupna@gmail.com>
2023-11-22 02:30:35 +0900

    로그인 디자인 4/4

--- front/src/pages/login/components/FindIdBox.tsx
+++ front/src/pages/login/components/FindIdBox.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
-
+import style from './FindIdBox.module.css';
 const FindIdBox = () => {
     return (
         <li>
-            findid
+            <div className={style.up}>
+                <div>
+                    <input className={style.input} type='email' placeholder='가입한 입력한 이메일 입력' />
+                    <button>발송</button>
+                </div>
+                <div>
+                    <input className={style.input} maxLength={6} minLength={6} />
+                    <button>확인</button>
+                </div>
+            </div>
+            <div className={style.down}>
+                <div>
+                    해당 이메일로 등록된 계정<br />
+                    [123456789012345] <br />
+                </div>
+                <div>
+                    이 계정의 비밀번호 재설정하기
+                </div>
+            </div>
         </li>
     );
 };
--- front/src/pages/login/components/ResetPwBox.tsx
+++ front/src/pages/login/components/ResetPwBox.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import style from './ResetPwBox.module.css';
 const ResetPwBox = () => {
     return (
         <li>
-            resetpw
+            <div className={style.content}>
+                <div className={style.notice}>
+                    [123456789012345]<br />
+                    계정의 비밀번호를 재설정합니다.
+                </div>
+
+                <input type='password' className={style.input} placeholder='*비밀번호 (6~20글자)'
+                    maxLength={20} minLength={6} />
+                <input type='password' className={style.input} placeholder='*비밀번호 확인'
+                    maxLength={20} minLength={6} />
+                <button className={style.btnModify}>
+                    변경하기
+                </button>
+            </div>
         </li>
     );
 };
