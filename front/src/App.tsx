c78df116303542c05452a877213323235d527ad1
Lekahelis <itnaupna@gmail.com>
2023-11-22 15:05:02 +0900

    인게임페이지 라우팅 추가

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Lobby from './pages/lobby/Lobby';
 import Login from './pages/login/Login';
+import Ingame from './pages/ingame/Ingame';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login/>}/>
       <Route path="/lobby" element={<Lobby />} />
+      <Route path="/room/:roomId" element={<Ingame/>}/>
     </Routes>
   );
 }
--- front/src/pages/ingame/Ingame.tsx
+++ front/src/pages/ingame/Ingame.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {useParams} from 'react-router-dom';
 
 const Ingame = () => {
+    const {roomId} = useParams();
     return (
         <div>
-            
+            {roomId}
         </div>
     );
 };
